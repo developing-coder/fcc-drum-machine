2fe9e1fcea00c01cb0c4ce12f85923c54212dc9d
Codebender <developingcoder@gmail.com>
2018-10-05 18:13:09 +0300

    add barebones Display and DrumKeys components

--- src/App.js
+++ src/App.js
@@ -5,23 +5,29 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className='App' id='drum-machine'>
+        <Display />
+        <DrumKeys />
+        Hello
       </div>
     );
+  }
+}
+
+class Display extends Component {
+  render() {
+    return <div id='display'>Display here!</div>
+  }
+}
+
+class DrumKeys extends Component {
+  render() {
+    const keyboard = ['Q', 'W', 'E', 'A', 'S', 'D', 'Z', 'X', 'C'];
+    const drumKeys = keyboard.map((letter) => {
+      return <div className='drum-pad'>{letter}</div>
+    })
+    
+    return <div id='drum-keys'>{drumKeys}</div>
   }
 }
 
